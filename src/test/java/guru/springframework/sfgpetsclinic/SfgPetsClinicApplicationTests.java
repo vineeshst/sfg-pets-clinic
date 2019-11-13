bb199d67da0ae0a169ff4e638bfbe999c02f94e4
vineeshst <vineeshstruts@gmail.com>
2019-11-13 13:29:19 +0100

    Initial Project Add

---
+++ src/test/java/guru/springframework/sfgpetsclinic/SfgPetsClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetsclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetsClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
