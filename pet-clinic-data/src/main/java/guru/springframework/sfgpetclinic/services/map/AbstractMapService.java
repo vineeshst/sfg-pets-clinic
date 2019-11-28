2fcac93322b84f6f4e4ad2e391619bfdf89c69cb
vineeshst <vineeshstruts@gmail.com>
2019-11-28 11:39:34 +0100

    Implement Spring Conf for Services. Closes #22

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -4,13 +4,11 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-
 public abstract class AbstractMapService<T, ID>{
     protected Map<ID, T> map = new HashMap<>();
     Set<T> findAll(){
         return new HashSet<>(map.values());
     }
-
     T findById(ID id){
         return map.get(id);
     }
