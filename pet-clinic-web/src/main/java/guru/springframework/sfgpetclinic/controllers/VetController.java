cb56c793e9a7ff527f4917a2ceed55c6c9be516e
vineeshst <vineeshstruts@gmail.com>
2019-12-06 16:20:36 +0100

    Menu Links are broken. Closes #45

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -4,16 +4,17 @@ import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-@RequestMapping({"/vets"})
+
 @Controller
 public class VetController {
-    private VetService vetService;
+    private final VetService vetService;
     public VetController(VetService vetService) {
         this.vetService = vetService;
     }
-    @RequestMapping({"","/","/index", "/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+
     public String listVets(Model model){
-        model.addAttribute("vets",vetService.findAll());
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
